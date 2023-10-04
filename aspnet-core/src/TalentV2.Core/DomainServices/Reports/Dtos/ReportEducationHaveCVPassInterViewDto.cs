5e996afb77fbfec4151c52335e19c5049fb16c09
Khanh Le <khanh.lesy@ncc.asia>
2023-10-04 10:19:57 +0700

    #51610:update passInterview

--- aspnet-core/src/TalentV2.Core/DomainServices/Reports/Dtos/ReportEducationHaveCVPassInterViewDto.cs
+++ aspnet-core/src/TalentV2.Core/DomainServices/Reports/Dtos/ReportEducationHaveCVPassInterViewDto.cs
@@ -9,11 +9,4 @@ namespace TalentV2.DomainServices.Reports.Dtos
     public class ReportEducationHaveCVPassInterViewDto : ReportEducationHaveCVPassTestDto
     {
     }
-    public class EducationPassInterViewDto
-    {
-        public string Name { get; set; }
-        public long Id { get; set; }
-        public long CVId { get; set; }
-        public string ColorCode { get; set; }
-    }
 }
