0e6c21669f32b10f82ed62e57ae3b345f43f150b
khanh-lesy <145412699+khanh-lesy@users.noreply.github.com>
2024-03-07 09:50:33 +0700

    #54919:update page view file CV (#86)

---
+++ angular/src/shared/pages/create-candidate/view-flles/view-flles.component.ts
@@ -0,0 +1,35 @@
+import { Component, Injector, OnInit, ViewChild } from '@angular/core';
+import { ToastMessageType } from '@shared/AppEnums';
+import { AppComponentBase } from '@shared/app-component-base';
+import { NgxDocViewerComponent } from 'ngx-doc-viewer';
+
+@Component({
+  selector: 'talent-view-flles',
+  templateUrl: './view-flles.component.html',
+  styleUrls: ['./view-flles.component.scss']
+})
+export class ViewFllesComponent extends AppComponentBase  implements OnInit {
+  documentUrl: string = '';
+  isLoading: boolean ;
+  @ViewChild('docViewer') docViewer: NgxDocViewerComponent;
+  constructor(injector: Injector,) {
+    super(injector);
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.documentUrl = params['documentUrl'];
+      if(this.documentUrl){
+       this.isLoading = true;
+      }
+     if(!this.documentUrl){
+       this.showToastMessage(ToastMessageType.ERROR, 'File does not exist!');
+      }
+   });
+  }
+
+  onDocumentLoad(): void {
+    this.isLoading = false;
+  }
+
+}
