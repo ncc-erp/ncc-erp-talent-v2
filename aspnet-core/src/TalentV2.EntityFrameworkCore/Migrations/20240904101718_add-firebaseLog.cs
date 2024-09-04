5aec2be651d3470df0c68c0d20f9a7d54fb1734d
levankydu <du.levanky@ncc.asia>
2024-09-04 17:36:08 +0700

    updated migration for adding tbFirebaseLog

---
+++ aspnet-core/src/TalentV2.EntityFrameworkCore/Migrations/20240904101718_add-firebaseLog.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TalentV2.Migrations
+{
+    public partial class addfirebaseLog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FirebaseCareerLogs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdFirebase = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FirebaseCareerLogs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FirebaseCareerLogs");
+        }
+    }
+}
