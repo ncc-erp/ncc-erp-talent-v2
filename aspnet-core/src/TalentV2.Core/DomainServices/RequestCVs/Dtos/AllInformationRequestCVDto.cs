012bde31e68825c9f2461f4c900aa1a6e4a0628f
tam.daonhon <tam.daonhon@ncc.asia>
2025-10-27 14:10:18 +0700

    Update accept offer mail template

--- aspnet-core/src/TalentV2.Core/DomainServices/RequestCVs/Dtos/AllInformationRequestCVDto.cs
+++ aspnet-core/src/TalentV2.Core/DomainServices/RequestCVs/Dtos/AllInformationRequestCVDto.cs
@@ -65,6 +65,7 @@ namespace TalentV2.DomainServices.RequestCVs.Dtos
         public string OnboardDate { get => DateTimeUtils.ToStringddMMyyyy(OnboardDateTime); }
         public float Salary { get; set; }
         public string LaunchAllowance => string.Format(System.Globalization.CultureInfo.GetCultureInfo("vi-VN"), "{0:#,##0}", TalentConstants.LAUNCH_ALLOWANCE);
+        public string InternalAccumulation => string.Format(System.Globalization.CultureInfo.GetCultureInfo("vi-VN"), "{0:#,##0}", TalentConstants.INTERNAL_ACCUMULATION);
         public string SalaryToString => string.Format(System.Globalization.CultureInfo.GetCultureInfo("vi-VN"), "{0:#,##0}", Salary);
         public string TotalSalary => string.Format(System.Globalization.CultureInfo.GetCultureInfo("vi-VN"), "{0:#,##0}", (TalentConstants.LAUNCH_ALLOWANCE + Salary));
         public string FullName { get; set; }
